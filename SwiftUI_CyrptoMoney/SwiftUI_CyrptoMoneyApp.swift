775fd8e9d63635c537bbe865904e5cc2b2d80de6
yusuf <ysfozturk3@gmail.com>
2021-11-18 12:04:32 +0100

    created

---
+++ SwiftUI_CyrptoMoney/SwiftUI_CyrptoMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CyrptoMoneyApp.swift
+//  SwiftUI_CyrptoMoney
+//
+//  Created by ysf on 18.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CyrptoMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
